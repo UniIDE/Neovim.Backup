6eaa01e77b8ed136290133b99a5c238dcb18d9a6
Your Name <you@example.com>
2026-06-29 15:58:12 +0100

    backup_volume

--- src/sh/lib/backup_volume.sh
+++ src/sh/lib/backup_volume.sh
@@ -1,9 +1,11 @@
+rsync_volume="rsync -av --checksum --progress --no-perms --no-owner --no-group" 
+
 from_backup_volume(){
-	rsync -av --progress --no-perms --no-owner --no-group $backup_volume/$tar_file $dir/
+	$rsync_volume $backup_volume/$tar_file $dir/
 	echo "Synced to Backup Volume"
 }
 
 to_backup_volume(){
-	rsync -av --progress --no-perms --no-owner --no-group $dir/$tar_file $backup_volume/
+	$rsync_volume $dir/$tar_file $backup_volume/
 	echo "Synced to Backup Volume"
 }
