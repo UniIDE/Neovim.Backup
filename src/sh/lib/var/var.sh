dc49f5fd9ec3196f8314a8335ec1400cfad3f4b1
Your Name <you@example.com>
2026-06-29 16:00:39 +0100

    backup_volume

--- src/sh/lib/var/var.sh
+++ src/sh/lib/var/var.sh
@@ -1,10 +1,10 @@
-dir=~/bac
-bac=neovim.bac
-tar_file=$bac.tgz
-bac_dir=$dir/$bac
-
 uni=~/.uni
 src=source
 uni_src=$uni/$src
+
+dir=$uni/bac
+bac=neovim.bac
+bac_dir=$dir/$bac
+tar_file=$bac.tgz
 
 backup_volume=/mnt/chromeos/removable/big/uni/bac
